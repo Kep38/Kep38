6c1fa99de5099da09be8dee2210e0d9f880ac555
Kep38 <148112989+Kep38@users.noreply.github.com>
2024-07-22 18:39:27 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,58 +1,44 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.78.0"
-    }
-  }
-}
-
 provider "yandex" {
-  service_account_key_file = var.service_account_key_file
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.zone
+  token     = "token"
+  cloud_id  = "cloud-id"
+  folder_id = "folder-id"
+  zone      = "ru-central1-a"
 }
-
 resource "yandex_compute_instance" "app" {
-  name  = "reddit-app-${count.index}"
-  count = 2  
-
+  name = "reddit-app-${count.index}"
+  count = var.app_count
   resources {
     cores  = 2
-    memory = 2
+    memory = 16
   }
-
   boot_disk {
     initialize_params {
-      image_id = var.image_id
+      # Óêàçàòü id îáðàçà ñîçäàííîãî â ïðåäûäóùåì äîìàøåì çàäàíèè
+      image_id = var.image_id	
     }
   }
-
   network_interface {
+    # Óêàçàí id ïîäñåòè default-ru-central1-a
     subnet_id = var.subnet_id
     nat       = true
   }
-
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
 
   connection {
-    type        = "ssh"
-    host        = self.network_interface.0.nat_ip_address
-    user        = "ubuntu"
-    agent       = false
+    type  = "ssh"
+    host  = self.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # ïóòü äî ïðèâàòíîãî êëþ÷à
     private_key = file(var.private_key_path)
   }
-
   provisioner "file" {
     source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
-
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
 }
-
